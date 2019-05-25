88fc6f6a2f23976a4a5d83035033622620065cd8
ilanbenyos <ilan_ben@hotmal.com>
2019-05-25 11:37:36 +0300

    before moving to members from owner/member

--- src/modules/v1/games/gameController.js
+++ src/modules/v1/games/gameController.js
@@ -11,19 +11,23 @@ export async function gameMove (ctx, next) {
     }
 
     const square= ctx.request.body.square;
-    const playerId1 = user._id.toString()
-    const player2Id = (playerId1 === game.owner._id.toString())?game.member._id:game.owner._id;
+
+    let gamePlayerObj = (user._id.toString() ===game.owner._id.toString())?game.owner:game.member;
+    const player2Id = (gamePlayerObj._id === game.owner._id.toString())?game.member._id:game.owner._id;
+
+
+    const player1 = await User.findById(gamePlayerObj._id);
     const player2 = await User.findById(player2Id);
 
-    const move = {player:user._id, square };
-    game.moves.push(move)
     let sq = game.board[square[0]][square[1]];
 
     if(sq !== null){
         ctx.throw(422,'SQUARE_NOT_EMPTY')
     }
+    const move = {player:user._id, square, symbol:gamePlayerObj.symbol };
+    game.moves.push(move)
     let tempBoard = _.cloneDeep(game.board);
-    tempBoard[square[0]][square[1]] = user.symbol;//====================
+    tempBoard[square[0]][square[1]] = gamePlayerObj.symbol;//====================
     game.board = tempBoard;
 
     game.nextPlayer = player2Id;
@@ -38,7 +42,7 @@ export async function gameMove (ctx, next) {
     try {
         await game.save();
         io.to(player2.socketId).emit('GAME_MOVE',{game});
-        io.to(player.socketId).emit('GAME_MOVE',{game});
+        io.to(player1.socketId).emit('GAME_MOVE',{game});
         ctx.body=ctx.body||{};
         ctx.body.game = game;
         if (next) { return next() }
